eb1c209222b1171e91e54a79d02a7123fc431ce1
kpamukcu <kapawebtasarim@gmail.com>
2025-04-07 21:41:12 +0300

    Create Admin Sablon Page

--- admin/sablon.php
+++ admin/sablon.php
@@ -15,6 +15,24 @@
 
 <body>
 
+<section id="adminPage">
+    <div class="container-fluid">
+        <div class="row bg-dark text-white">
+            <div class="col-md-2">Admin / Proje Adı</div>
+            <div class="col-md-10 text-end">
+                <a href="logout.php" class="text-white text-decoration-none">Güvenli Çıkış</a>
+            </div>
+        </div>
+        <div class="row" style="height: 96.5vh;">
+            <div class="col-md-2 bg-dark py-3" id="adminNav">
+                <a href="dashboard.php">Başlangıç</a><br>
+                <a href="logout.php" class="text-warning">Güvenli Çıkış</a>
+            </div>
+            <div class="col-md-10 bg-light py-3">Admin Body</div>
+        </div>
+    </div>
+</section>
+
 
 <script src="../assets/js/bootstrap.bundle.min.js"></script>
 </body>
