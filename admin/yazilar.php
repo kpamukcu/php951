49637ee90d19ed25188bd15bb0607f2fdfb98c52
kpamukcu <kapawebtasarim@gmail.com>
2025-04-21 21:57:25 +0300

    add content module

---
+++ admin/yazilar.php
@@ -0,0 +1,28 @@
+<?php 
+require_once('header.php'); 
+?>
+<!-- Admin Body Section Start -->
+<div class="row">
+    <div class="col-md-6">
+        <h3>Yazılar</h3>
+    </div>
+    <div class="col-md-6 text-end">
+        <a href="yazi-ekle.php" class="btn btn-info text-white">Yeni Yazı Ekle</a>
+    </div>
+</div>
+<div class="row mt-3">
+    <div class="col-12">
+        <table class="table table-striped">
+            <tr>
+                <th>Başlık 1</th>
+                <th>Başlık 2</th>
+                <th>Başlık 3</th>
+                <th>Başlık 4</th>
+                <th>Başlık 5</th>
+                <th>Başlık 6</th>
+            </tr>
+        </table>
+    </div>
+</div>
+<!-- Admin Body Section End -->
+<?php require_once('footer.php'); ?>
--- aribilgi.sql
+++ aribilgi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: 127.0.0.1
--- Üretim Zamanı: 16 Nis 2025, 20:39:06
+-- Üretim Zamanı: 21 Nis 2025, 20:56:14
 -- Sunucu sürümü: 10.4.32-MariaDB
 -- PHP Sürümü: 8.2.12
 
@@ -46,6 +46,30 @@ INSERT INTO `kategoriler` (`id`, `katAdi`, `katTuru`, `ustKat`, `aciklama`) VALU
 (5, 'Html', 'Alt Kategori', 'Grafik Tasarımı', 'Html Kodları Hakkında Blog Yazısı'),
 (6, 'Seo', 'Alt Kategori', 'Dijital Pazarlama', 'Arama Motoru Optimizasyonu Blog Yazıları');
 
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `yazilar`
+--
+
+CREATE TABLE `yazilar` (
+  `id` int(11) NOT NULL,
+  `baslik` varchar(150) NOT NULL,
+  `aciklama` text NOT NULL,
+  `meta` varchar(250) NOT NULL,
+  `durum` varchar(15) NOT NULL,
+  `tarih` varchar(15) NOT NULL,
+  `kategori` varchar(50) NOT NULL,
+  `gorsel` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Tablo döküm verisi `yazilar`
+--
+
+INSERT INTO `yazilar` (`id`, `baslik`, `aciklama`, `meta`, `durum`, `tarih`, `kategori`, `gorsel`) VALUES
+(1, 'Deneme', '<p>deneme</p>', 'deneme', 'Yayında', '2025-04-21', 'Web Tasarımı', '../assets/img/ari-bilisim-banner-800x800.gif');
+
 --
 -- Dökümü yapılmış tablolar için indeksler
 --
@@ -57,6 +81,12 @@ ALTER TABLE `kategoriler`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Tablo için indeksler `yazilar`
+--
+ALTER TABLE `yazilar`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
 --
 
@@ -65,6 +95,12 @@ ALTER TABLE `kategoriler`
 --
 ALTER TABLE `kategoriler`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `yazilar`
+--
+ALTER TABLE `yazilar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
