128315dd818532a2191beb424d5be4a19d5875ef
kpamukcu <kapawebtasarim@gmail.com>
2025-04-22 11:22:57 +0300

    api example

---
+++ assets/api.php
@@ -0,0 +1,18 @@
+<?php
+
+header('Content-Type:application/json');
+require_once('./assets/baglan.php');
+
+$kategoriler = $db->prepare('select * from kategoriler');
+$kategoriler->execute();
+$kategorilerSatir = $kategoriler->fetchAll(PDO::FETCH_ASSOC);
+//$kategorilerSatir = $kategoriler->fetchAll(PDO::FETCH_OBJ);
+
+echo json_encode($kategorilerSatir);
+
+/*
+Özellik                 | PDO::FETCH_ASSOC  | PDO::FETCH_OBJ
+Erişim tipi             | $veri["alanAdi"]  | $veri->alanAdi
+json_encode() çıktısı   | Aynı görünür      | Aynı görünür
+Performans              | Hemen hemen aynı  | Aynı
+*/
