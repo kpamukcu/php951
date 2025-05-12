7a5460e4a090b328e19cdce79d6f9ba5a45adc86
kpamukcu <kapawebtasarim@gmail.com>
2025-05-12 21:44:59 +0300

    Article List, Article Page Start

--- aside.php
+++ aside.php
@@ -1,3 +1,37 @@
 <div class="col-md-3">
-    Burası Aside
+    <div class="row">
+        <div class="col-12">
+            <div class="card shadow">
+                <div class="card-body">
+                    <form action="" method="get">
+                        <input type="search" name="ara" placeholder="Kelime Girin..." class="form-control mb-2">
+                        <input type="submit" value="Ara" class="btn btn-purple w-100">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="text-purple">Kategoriler</h3>
+                    <div class="flexList">
+                        <?php
+                        $katList = $db->prepare('select distinct kategori from yazilar');
+                        $katList->execute();
+
+                        if ($katList->rowCount()) {
+                            foreach ($katList as $katListSatir) {
+                        ?>
+                                <a href="kategori.php?katAdi=<?php echo $katListSatir['kategori']; ?>"><?php echo $katListSatir['kategori']; ?></a>
+                        <?php
+                            }
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
--- makale.php
+++ makale.php
@@ -1,34 +1,36 @@
-<?php 
-require_once('header.php'); 
+<?php
+require_once('header.php');
 
-if(isset($_GET['postID'])){
+if (isset($_GET['postID'])) {
     $id = $_GET['postID'];
-    $article = $db -> prepare('select * from yazilar where id=?');
-    $article -> execute(array($id));
-    $articleSatir = $article -> fetch();
+    $article = $db->prepare('select * from yazilar where id=?');
+    $article->execute(array($id));
+    $articleSatir = $article->fetch();
 }
 
 ?>
 
 <!-- Content Section Start -->
- <section id="article">
+<section id="article" class="py-5">
     <div class="container">
         <div class="row">
             <div class="col-md-9">
                 <!-- Breadcrumb Start -->
-                 <div>
+                <div class="mb-3">
                     <a href="index.php">Ana Sayfa</a> / <a href="blog.php">Blog</a> / <?php echo $articleSatir['baslik']; ?>
                 </div>
                 <!-- Breadcrumb End -->
-                 <img src="<?php echo substr($articleSatir['gorsel'],1); ?>" alt="<?php echo $articleSatir['baslik']; ?>">
-                 <h1><?php echo $articleSatir['baslik']; ?></h1>
-                 <small>Tarih: <?php echo $articleSatir['tarih']; ?> / XX Yorum</small>
-                XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+                <img src="<?php echo substr($articleSatir['gorsel'], 1); ?>" alt="<?php echo $articleSatir['baslik']; ?>" class="border shadow w-100">
+                <div class="my-4">
+                    <h1><?php echo $articleSatir['baslik']; ?></h1>
+                    <small>Tarih: <?php echo $articleSatir['tarih']; ?> / XX Yorum</small>
+                </div>
+                <?php echo $articleSatir['aciklama']; ?>
             </div>
             <?php require_once('aside.php'); ?>
         </div>
     </div>
- </section>
+</section>
 <!-- Content Section End -->
 
 <?php require_once('footer.php'); ?>
